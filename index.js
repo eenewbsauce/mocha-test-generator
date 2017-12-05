c199579cec07590c5a191716adaeeaf0922069a6
eenewbsauce <ryan.m.tracey@gmail.com>
2017-12-05 10:01:56 -0500

    fixed untracked files

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const argv = require('minimist')(process.argv.slice(2));
+const { helpers, mocha } = require('./lib');
 
 let sourceClass = require(argv.source);
+let className = sourceClass.name;
+let output = '';
+let specs = [];
 
 Reflect.ownKeys(sourceClass.prototype).forEach((key) => {
-    console.log(sourceClass.prototype[key])
-    console.log(sourceClass.prototype[key].toString())
-})
+    // console.log(helpers.getMethodName(sourceClass.prototype[key]));
+    // console.log(sourceClass.prototype[key].toString())
+    specs.push(mocha.it(sourceClass, key));
+
+});
+
+output = mocha.describe(className, specs, false);
+
+console.log(JSON.stringify(output, null, 4));
